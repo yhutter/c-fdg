f6161a7445c81440f57c0b4e3b11551e094b0cb6
yhutter <hutteryannick@gmail.com>
2025-02-09 15:43:13 +0100

    Add springs

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 
 #define BACKGROUND_COLOR (Color){0x18, 0x18, 0x18, 0xff}
 #define BLOB_COLOR (Color){0xff, 0xdd, 0x33, 0xff}
+#define K 0.005f
 
 typedef struct {
     Vector2 pos;
@@ -11,6 +12,38 @@ typedef struct {
     Color color;
     float size;
 } blob_t;
+
+typedef struct {
+    float length;
+    blob_t* b1;
+    blob_t* b2;
+} spring_t;
+
+
+spring_t create_spring(float length, blob_t* b1, blob_t* b2) {
+    spring_t spring = {
+        .length = length,
+        .b1 = b1,
+        .b2 = b2
+    };
+    return spring;
+}
+
+void accelerate_blob(blob_t* blob, Vector2 f) {
+    // f = m * a -> a = f / m (note that mass is 1
+    blob->acc = Vector2Add(blob->acc, f);
+}
+
+void spring_move_blobs(spring_t* s) {
+    Vector2 f = Vector2Subtract(s->b2->pos, s->b1->pos);
+    float mag = K * (Vector2Length(f) - s->length);
+    f = Vector2Scale(f, mag);
+    f = Vector2Normalize(f);
+
+    accelerate_blob(s->b1, f);
+    f = Vector2Scale(f, -1.0f);
+    accelerate_blob(s->b2, f);
+}
 
 
 blob_t create_blob(Vector2 pos, float size, Color color) {
@@ -24,10 +57,6 @@ blob_t create_blob(Vector2 pos, float size, Color color) {
     return blob;
 }
 
-void accelerate_blob(blob_t* blob, Vector2 f) {
-    // f = m * a -> a = f / m (note that mass is 1
-    blob->acc = Vector2Add(blob->acc, f);
-}
 
 void update_blob_position(blob_t* blob) {
     blob->vel = Vector2Add(blob->vel, blob->acc);
@@ -47,16 +76,20 @@ int main(void) {
     Camera2D camera = {0};
     camera.zoom = 1.0f;
 
-    blob_t blob = create_blob((Vector2){ 640.0f, 320.f }, 10.0f, BLOB_COLOR);
+    blob_t blob1 = create_blob((Vector2){ 320.0f, 320.f }, 10.0f, BLOB_COLOR);
+    blob_t blob2 = create_blob((Vector2){ 620.0f, 320.f }, 10.0f, BLOB_COLOR);
+    spring_t spring = create_spring(150.0f, &blob1, &blob2);
 
 
     while (!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground(BACKGROUND_COLOR);
             BeginMode2D(camera);
-                accelerate_blob(&blob, (Vector2){0.0, 0.9f});
-                update_blob_position(&blob);
-                render_blob(&blob);
+                spring_move_blobs(&spring);
+                update_blob_position(&blob1);
+                update_blob_position(&blob2);
+                render_blob(&blob1);
+                render_blob(&blob2);
             EndMode2D();
         EndDrawing();
     }
